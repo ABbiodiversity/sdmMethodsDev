f332b4d839e44c391122ec7e3d6fb0280b5a52d6
bgcasey <brendangerardcasey@gmail.com>
2026-09-06 05:08:20 -0600

    Add initial scratch workspace for testing and exploration

---
+++ 1_code/_setup/_scratch.R
@@ -0,0 +1,20 @@
+# ---
+# title: Scratch Work
+# author: Brendan Casey
+# created: 2026-09-06
+# inputs:
+#   - ad hoc, temporary inputs as needed
+# outputs:
+#   - none by default
+# notes:
+#   Temporary workspace for testing, exploration, and one-off
+#   tasks. Keep work grouped under dated headings and move any
+#   durable code into the appropriate script or module.
+#
+#   Add temporary code under the relevant date heading. Create a
+#   new heading for each day of scratch work.
+# ---
+
+# 2026-09-06 ----
+
+# End of script ----
